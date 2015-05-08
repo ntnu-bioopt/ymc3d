fdf135c6da7a13ee3a14831ef3ffe48119184427
Asgeir Bjørgan <asgeirbj@samfundet.no>
2015-05-08 13:49:14 +0200

    Corrected run_mc3d-function.

--- src/mc3d_gpu.h
+++ src/mc3d_gpu.h
@@ -2,9 +2,10 @@
 #define MC3D_GPU_H_DEFINED
 
 #include "mc3d_types.h"
+#include "mc3d_rng.h"
 
 //MAIN TRACKER FUNCTION
-void run_3dmc_gpu(Geometry geometry, OpticalProps optProps, int num_photons, double *R, double *R_tot, double *T, double *T_tot, float *A, int *num_finished_photons);
+void run_3dmc_gpu(geometry_t geometry, opticalprops_t optProps, int num_photons, double *R, double *R_tot, double *T, double *T_tot, float *A, int *num_finished_photons);
 
 //HELPER FUNCTIONS
 
